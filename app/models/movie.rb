23e8a1f72fe12cce5b003763f66672c369cdccd2
Marley Watson <marleykw17@gmail.com>
2023-03-22 16:27:15 -0400

    commit tree fb083debe724b2b634e1d4ab5f3992962d6e466e

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,28 @@
 class Movie < ActiveRecord::Base
+    def self.create_with_title(title)
+        Movie.create(title:title)
+    end
+
+    def self.first_movie
+        Movie.first
+    end 
+
+    def self.last_movie
+        Movie.last
+    end 
+
+    def self.movie_count
+        Movie.count
+    end 
+
+    def self.find_movie_with_id(id)
+        Movie.find_by id:id
+    end
+
+
+    def self.find_movies_after_2002
+        Movie.where(:release_date > 2002)
+    end
+
 
 end
